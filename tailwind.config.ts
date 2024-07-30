daabe14236776d8c6ee58b46aa5fe513c959274c
Lincoln Cavalieri <ljcavali@gmail.com>
2024-07-30 00:57:45 -0500

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,24 +7,22 @@ const config: Config = {
         './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     ],
     theme: {
-        extend: {
-            colors: {
-                'bg-black': '#0b0b0b',
-            },
+            extend: {
+              colors: {
+                black: '#0b0b0b', // Overwrite the default black color
+                standard: {
+                  1: '#111222',
+                  2: '#394365',
+                  3: '#535B75',
+                  4: '#969FBC',
+                  5: '#CAD0DD',
+                  6: '#DCE0EA',
+                  7: '#F2F4F9',
+                },
+              },
             backgroundImage: {
                 'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
                 'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-            },
-            colors: {
-                standard: {
-                    1: '#111222',
-                    2: '#394365',
-                    3: '#535B75',
-                    4: '#969FBC',
-                    5: '#CAD0DD',
-                    6: '#DCE0EA',
-                    7: '#F2F4F9',
-                },
             },
             fontFamily: {
                 'ibm-mono': ['"IBM Plex Mono"', 'monospace'],
